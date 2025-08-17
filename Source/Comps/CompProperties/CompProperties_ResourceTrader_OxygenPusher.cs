ca243227209933c9c2a997d7337350d082379927
SokyranTheDragon <exploder1531@gmail.com>
2025-08-17 23:27:27 +0200

    Replace vanilla low power mode for oxygen pump with our own

--- Source/Comps/CompProperties/CompProperties_ResourceTrader_OxygenPusher.cs
+++ Source/Comps/CompProperties/CompProperties_ResourceTrader_OxygenPusher.cs
@@ -5,6 +5,8 @@ namespace VanillaGravshipExpanded;
 
 public class CompProperties_ResourceTrader_OxygenPusher : CompProperties_ResourceTrader
 {
+    // Our custom properties
+    public float lowPowerConsumptionFactor = 0.1f;
     // Vanilla CompProperties_OxygenPusher properties
     public bool requiresPower = true;
     public float airPerSecondPerHundredCells = 0.1f;
--- Source/Comps/CompResourceTrader_OxygenPusher.cs
+++ Source/Comps/CompResourceTrader_OxygenPusher.cs
@@ -6,39 +6,71 @@ namespace VanillaGravshipExpanded;
 
 public class CompResourceTrader_OxygenPusher : CompResourceTrader
 {
+    private bool lowPowerMode = false;
     private CompPowerTrader intPowerTrader;
 
     protected new CompProperties_ResourceTrader_OxygenPusher Props => (CompProperties_ResourceTrader_OxygenPusher)props;
 
     public CompPowerTrader PowerTrader => intPowerTrader ??= parent.GetComp<CompPowerTrader>();
 
+    public override void PostSpawnSetup(bool respawningAfterLoad)
+    {
+        base.PostSpawnSetup(respawningAfterLoad);
+
+        if (parent.Map?.Biome.inVacuum != true)
+            EnableLowPowerMode();
+    }
+
     public override void CompTickRare()
     {
-        // Never work in a vacuum biome
+        base.CompTickRare();
+
+        // Never work in a non-vacuum biome
         if (parent.Map?.Biome.inVacuum != true)
             return;
 
-        // If disabled due to no more vacuum/exposed to vacuum, skip most other checks
-        // TODO: Actually do this part
-
-        // Don't do anything if no pipe net or turned off
-        if (PipeNet == null || !ResourceOn)
-            return;
         // Make sure the power is on
         if (Props.requiresPower && PowerTrader.Off)
             return;
+
+        // Don't do anything if no pipe net or turned off
+        if (PipeNet == null || !ResourceOn)
+        {
+            EnableLowPowerMode();
+            return;
+        }
         // Don't do anything if there's not enough oxygen stored
         var available = PipeNet.TotalProductionThisTick + PipeNet.Stored - PipeNet.TotalConsumptionThisTick;
         if (available <= 0f)
+        {
+            EnableLowPowerMode();
             return;
+        }
+
+        // If disabled due to no more vacuum/exposed to vacuum, skip most other checks
+        // TODO: Actually do this part
+        if (lowPowerMode)
+        {
+            var r = parent.GetRoom();
+            if (r.ExposedToSpace || r.Vacuum <= 0)
+                return;
+
+            DisableLowPowerMode();
+        }
 
         var room = parent.GetRoom();
         if (room.ExposedToSpace)
+        {
+            EnableLowPowerMode();
             return;
+        }
 
         var vacuum = room.Vacuum;
         if (vacuum <= 0)
+        {
+            EnableLowPowerMode();
             return;
+        }
 
         var maxConsumption = Props.consumptionPerTick / 100f * GenTicks.TickRareInterval;
         var consumption = maxConsumption;
@@ -59,6 +91,9 @@ public class CompResourceTrader_OxygenPusher : CompResourceTrader
 
     public override string CompInspectStringExtra()
     {
+        return string.Empty;
+
+        // TODO: Finish working on this
         if (parent.Spawned && parent.Map.Biome.inVacuum)
         {
             var room = parent.GetRoom();
@@ -70,5 +105,25 @@ public class CompResourceTrader_OxygenPusher : CompResourceTrader
         }
 
         return string.Empty;
+    }
+
+    private void EnableLowPowerMode()
+    {
+        if (lowPowerMode)
+            return;
+
+        lowPowerMode = true;
+        if (Props.requiresPower)
+            PowerTrader.PowerOutput = PowerTrader.Props.PowerConsumption * Props.lowPowerConsumptionFactor;
+    }
+
+    private void DisableLowPowerMode()
+    {
+        if (!lowPowerMode)
+            return;
+
+        lowPowerMode = false;
+        if (Props.requiresPower)
+            PowerTrader.PowerOutput = PowerTrader.Props.PowerConsumption;
     }
 }
