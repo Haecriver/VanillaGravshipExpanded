600dee3db5b8976c976aad44703b4d76b35c8f12
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-12 17:01:57 +0200

    Tweaks to grav anomaly colors

--- Source/GameConditions/GameCondition_GravitationalAnomaly.cs
+++ Source/GameConditions/GameCondition_GravitationalAnomaly.cs
@@ -4,36 +4,50 @@ using LudeonTK;
 using RimWorld;
 using UnityEngine;
 using Verse;
+using Verse.Noise;
+using Verse.Sound;
 namespace VanillaGravshipExpanded
 {
     public class GameCondition_GravitationalAnomaly : GameCondition
     {
-        [StaticConstructorOnStartup]
-        private class GravAnomalyOverlay : SkyOverlay
+        /* [StaticConstructorOnStartup]
+         private class GravAnomalyOverlay : SkyOverlay
+         {
+             private static readonly Material GravAnomalyOverlayWorld = MatLoader.LoadMat("Weather/GlowSporeOverlayWorld");
+
+             private static readonly ComplexCurve speedCurve = new ComplexCurve(new UnityEngine.Keyframe(0f, 0f), new UnityEngine.Keyframe(1f, 1f), new UnityEngine.Keyframe(2f, 1.1f), new UnityEngine.Keyframe(3f, 1.2f));
+
+             private TexturePannerSpeedCurve panner0 = new TexturePannerSpeedCurve(GravAnomalyOverlayWorld, "_MainTex", speedCurve, new Vector2(-1f, -0.2f), 0.0001f);
+
+             private TexturePannerSpeedCurve panner1 = new TexturePannerSpeedCurve(GravAnomalyOverlayWorld, "_MainTex2", speedCurve, new Vector2(0.35f, -1f), 5E-05f);
+
+             public override void SetOverlayColor(Color color)
+             {
+                 GravAnomalyOverlayWorld.color = color;
+             }
+
+             public override void DrawOverlay(Map map)
+             {
+                 SkyOverlay.DrawWorldOverlay(map, GravAnomalyOverlayWorld);
+             }
+
+             public override void TickOverlay(Map map, float lerpFactor)
+             {
+                 panner0.Tick();
+                 panner1.Tick();
+             }
+         }*/
+
+        private static readonly WeatherOverlay_GravitationalAnomaly gravitationalAnomalyOverlay = new WeatherOverlay_GravitationalAnomaly();
+
+        private static readonly List<SkyOverlay> overlays = new List<SkyOverlay> { gravitationalAnomalyOverlay };
+
+
+        private Sustainer sustainer;
+
+        public override List<SkyOverlay> SkyOverlays(Map map)
         {
-            private static readonly Material GravAnomalyOverlayWorld = MatLoader.LoadMat("Weather/GlowSporeOverlayWorld");
-
-            private static readonly ComplexCurve speedCurve = new ComplexCurve(new UnityEngine.Keyframe(0f, 0f), new UnityEngine.Keyframe(1f, 1f), new UnityEngine.Keyframe(2f, 1.1f), new UnityEngine.Keyframe(3f, 1.2f));
-
-            private TexturePannerSpeedCurve panner0 = new TexturePannerSpeedCurve(GravAnomalyOverlayWorld, "_MainTex", speedCurve, new Vector2(-1f, -0.2f), 0.0001f);
-
-            private TexturePannerSpeedCurve panner1 = new TexturePannerSpeedCurve(GravAnomalyOverlayWorld, "_MainTex2", speedCurve, new Vector2(0.35f, -1f), 5E-05f);
-
-            public override void SetOverlayColor(Color color)
-            {
-                GravAnomalyOverlayWorld.color = color;
-            }
-
-            public override void DrawOverlay(Map map)
-            {
-                SkyOverlay.DrawWorldOverlay(map, GravAnomalyOverlayWorld);
-            }
-
-            public override void TickOverlay(Map map, float lerpFactor)
-            {
-                panner0.Tick();
-                panner1.Tick();
-            }
+            return overlays;
         }
 
         private int curColorIndex = -1;
@@ -49,10 +63,6 @@ namespace VanillaGravshipExpanded
         private const int TransitionDurationTicks_NotPermanent = 280;
 
         private int longTickCounter = 0;
-
-        private static readonly GravAnomalyOverlay gravAnomalyOverlay = new GravAnomalyOverlay();
-
-        private static readonly List<SkyOverlay> overlays = new List<SkyOverlay> { gravAnomalyOverlay };
 
         private static readonly Color[] Colors = new Color[8]
         {
@@ -89,6 +99,10 @@ namespace VanillaGravshipExpanded
             curColorTransition = 1f;
         }
 
+        public override void GameConditionDraw(Map map)
+        {
+            gravitationalAnomalyOverlay.DrawOverlay(map);
+        }
 
         public override float SkyTargetLerpFactor(Map map)
         {
@@ -104,16 +118,10 @@ namespace VanillaGravshipExpanded
 
 
 
-        public override void GameConditionDraw(Map map)
-        {
-            if (!HiddenByOtherCondition(map))
-            {
-                gravAnomalyOverlay.DrawOverlay(map);
-            }
-        }
 
         public override void GameConditionTick()
         {
+            var map = SingleMap;
             curColorTransition += 1f / (float)TransitionDurationTicks;
             if (curColorTransition >= 1f)
             {
@@ -121,13 +129,7 @@ namespace VanillaGravshipExpanded
                 curColorIndex = GetNewColorIndex();
                 curColorTransition = 0f;
             }
-            foreach (Map affectedMap in base.AffectedMaps)
-            {
-                if (!HiddenByOtherCondition(affectedMap))
-                {
-                    gravAnomalyOverlay.TickOverlay(affectedMap, 1f);
-                }
-            }
+            gravitationalAnomalyOverlay.TickOverlay(map, 1f);
             longTickCounter++;
             if (longTickCounter >= 2000)
             {
@@ -147,9 +149,6 @@ namespace VanillaGravshipExpanded
                     select x).RandomElement();
         }
 
-        public override List<SkyOverlay> SkyOverlays(Map map)
-        {
-            return overlays;
-        }
+       
     }
 }
---
+++ Source/Weather/WeatherOverlay_GravitationalAnomaly.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using Verse;
+
+namespace VanillaGravshipExpanded
+{
+    [HotSwappable]
+    public class WeatherOverlay_GravitationalAnomaly : WeatherOverlayDualPanner
+    {
+        public WeatherOverlay_GravitationalAnomaly()
+        {
+            Init();
+        }
+
+        public void Init()
+        {
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+              
+
+                worldOverlayMat = new Material(MaterialPool.MatFrom("Weather/MicrometeorStormWorldOverlay"));
+                var mat = MatLoader.LoadMat("Weather/GlowSporeOverlayWorld");
+                worldOverlayMat.CopyPropertiesFromMaterial(mat);
+                worldOverlayMat.shader = mat.shader;
+                Texture2D texture = ContentFinder<Texture2D>.Get("Weather/MicrometeorStormWorldOverlay");
+                worldOverlayMat.SetTexture("_MainTex", texture);
+                worldOverlayMat.SetTexture("_MainTex2", texture);
+                worldOverlayMat.SetColor("_TuningColor", new Color(0.4f,0.53f,1f, 0.05f));
+                worldOverlayMat.SetColor("_Color", new Color(0.4f, 0.53f, 1f, 0.05f));
+                worldOverlayMat.SetColor("_MainTexColor", new Color(0.4f, 0.53f, 1f, 0.05f));
+                worldOverlayMat.SetColor("_MainTex2Color", new Color(0.4f, 0.53f, 1f, 0.05f));
+
+
+
+            });
+        }
+    }
+}
