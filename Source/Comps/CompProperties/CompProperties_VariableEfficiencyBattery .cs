8121404e7742badc1e989e3ba138c2d68feba1e7
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-09-12 15:15:01 +0200

    Maintenance hub and capacitor harmonizer

---
+++ Source/Comps/CompProperties/CompProperties_VariableEfficiencyBattery .cs
@@ -0,0 +1,13 @@
+﻿using RimWorld;
+namespace VanillaGravshipExpanded
+{
+    public class CompProperties_VariableEfficiencyBattery : CompProperties_Battery
+    {
+
+
+        public CompProperties_VariableEfficiencyBattery()
+        {
+            compClass = typeof(CompVariableEfficiencyBattery);
+        }
+    }
+}
---
+++ Source/Comps/CompVariableEfficiencyBattery.cs
@@ -0,0 +1,109 @@
+﻿
+﻿using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace VanillaGravshipExpanded
+{
+    public class CompVariableEfficiencyBattery : CompPowerBattery
+    {
+
+        CompAffectedByFacilities affectedBy;
+
+        public float Efficiency
+        {
+            get
+            {
+                List<Thing> linkedFacilitiesListForReading = affectedBy.LinkedFacilitiesListForReading;
+                for (int i = 0; i < linkedFacilitiesListForReading.Count; i++)
+                {
+                    if (linkedFacilitiesListForReading[i].def == VGEDefOf.VGE_CapacitorHarmonizer)
+                    {
+                        return 1;
+                    }
+                }
+                return Props.efficiency;
+            }
+
+        }
+
+        public float SelfDischarge
+        {
+            get
+            {
+
+                if (Efficiency == 1)
+                {
+                    return 0;
+                }
+
+                return 10;
+            }
+
+        }
+
+        public new float AmountCanAccept
+        {
+            get
+            {
+                if (parent.IsBrokenDown() || StunnedByEMP)
+                {
+                    return 0f;
+                }
+                CompProperties_Battery compProperties_Battery = Props;
+                return (compProperties_Battery.storedEnergyMax - storedEnergy) / Efficiency;
+            }
+        }
+
+        public new CompProperties_VariableEfficiencyBattery Props => (CompProperties_VariableEfficiencyBattery)props;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            stunnableComp = parent.GetComp<CompStunnable>();
+            affectedBy = parent.GetComp<CompAffectedByFacilities>();
+        }
+
+        public override void CompTick()
+        {
+            if (Efficiency != 1)
+            {
+                DrawPower(Mathf.Min(SelfDischarge * CompPower.WattsToWattDaysPerTick, storedEnergy));
+            }
+
+        }
+
+
+        public new void AddEnergy(float amount)
+        {
+            if (amount < 0f)
+            {
+                Log.Error("Cannot add negative energy " + amount);
+            }
+            else if (!StunnedByEMP)
+            {
+                if (amount > AmountCanAccept)
+                {
+                    amount = AmountCanAccept;
+                }
+                amount *= Efficiency;
+                storedEnergy += amount;
+            }
+        }
+
+        public override string CompInspectStringExtra()
+        {
+
+            string text = "PowerBatteryStored".Translate() + ": " + storedEnergy.ToString("F0") + " / " + Props.storedEnergyMax.ToString("F0") + " Wd";
+            text += "\n" + "PowerBatteryEfficiency".Translate() + ": " + (Efficiency * 100f).ToString("F0") + "%";
+            if (storedEnergy > 0f)
+            {
+                text += "\n" + "SelfDischarging".Translate() + ": " + SelfDischarge.ToString("F0") + " W";
+            }
+            return text;
+        }
+
+
+    }
+}
