b3f68e151668e45f27c4607b9b6fd4b0c2ef8a75
SokyranTheDragon <exploder1531@gmail.com>
2025-09-13 22:12:41 +0200

    Grav field extenders work with all engines

---
+++ Source/HarmonyPatches/PlaceWorker_InRangeOfGravEngine_AllowsPlacing_Patch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(PlaceWorker_InRangeOfGravEngine), nameof(PlaceWorker_InRangeOfGravEngine.AllowsPlacing))]
+public class PlaceWorker_InRangeOfGravEngine_AllowsPlacing_Patch
+{
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, MethodBase baseMethod)
+        => PlaceWorker_InRangeOfGravEngine_DrawGhost_Patch.ListAllAllowedGravEngines(instr, baseMethod, 1);
+}
---
+++ Source/HarmonyPatches/PlaceWorker_InRangeOfGravEngine_DrawGhost_Patch.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(PlaceWorker_InRangeOfGravEngine), nameof(PlaceWorker_InRangeOfGravEngine.DrawGhost))]
+public class PlaceWorker_InRangeOfGravEngine_DrawGhost_Patch
+{
+    private static List<Thing> TmpWorkingList = [];
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr, MethodBase baseMethod)
+        => ListAllAllowedGravEngines(instr, baseMethod, 0);
+
+    internal static IEnumerable<CodeInstruction> ListAllAllowedGravEngines(IEnumerable<CodeInstruction> instr, MethodBase baseMethod, int gravshipFacilityPropsIndex)
+    {
+        var targetMethod = typeof(ListerThings).DeclaredMethod(nameof(ListerThings.ThingsOfDef));
+        var replacementMethod = typeof(PlaceWorker_InRangeOfGravEngine_DrawGhost_Patch).DeclaredMethod(nameof(AllLinkableBuildings));
+
+        var patchedMethods = 0;
+
+        foreach (var ci in instr)
+        {
+            if (ci.Calls(targetMethod))
+            {
+                yield return CodeInstruction.LoadLocal(gravshipFacilityPropsIndex);
+
+                ci.opcode = OpCodes.Call;
+                ci.operand = replacementMethod;
+
+                patchedMethods++;
+            }
+
+            yield return ci;
+        }
+
+        const int expectedPatches = 1;
+        if (patchedMethods != expectedPatches)
+            Log.Error($"Patching {baseMethod.DeclaringType}:{baseMethod.Name} - unexpected amount of patches. Expected patches: {expectedPatches}, actual patch amount: {patchedMethods}. There may be bugs with linking some buildings to non-vanilla grav engines.");
+    }
+
+    private static List<Thing> AllLinkableBuildings(ListerThings lister, ThingDef _, CompProperties_GravshipFacility comp)
+    {
+        TmpWorkingList.Clear();
+
+        foreach (var building in comp.linkableBuildings)
+            TmpWorkingList.AddRange(lister.ThingsOfDef(building));
+
+        return TmpWorkingList;
+    }
+}
