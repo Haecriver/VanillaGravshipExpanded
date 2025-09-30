986f206b3297ac6dabf966940fbc9350cfcb7730
SokyranTheDragon <exploder1531@gmail.com>
2025-09-30 18:11:13 +0200

    Prevent building grav engines if another one exists

---
+++ Source/PlaceWorkers/PlaceWorker_OnlySingleGravEngine.cs
@@ -0,0 +1,21 @@
+﻿using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+public class PlaceWorker_OnlySingleGravEngine : PlaceWorker
+{
+    public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+    {
+        // Only check if thing is null. We don't want to prevent the player from reinstalling the engine they already have.
+        if (thing == null)
+        {
+            if (GravshipUtility.PlayerHasGravEngine())
+                return "VGE_SingleGravEngineOnly".Translate(checkingDef);
+            if (GravshipHelper.PlayerHasGravEngineUnderConstruction())
+                return "VGE_SingleGravEngineBlueprintOnly".Translate(checkingDef);
+        }
+
+        return true;
+    }
+}
