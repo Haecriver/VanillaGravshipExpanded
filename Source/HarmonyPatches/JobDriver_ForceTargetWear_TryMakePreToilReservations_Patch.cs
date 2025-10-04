edfa715bdb99932053a7807a731dbf0fc2c2cfea
SokyranTheDragon <exploder1531@gmail.com>
2025-10-04 17:47:42 +0200

    Add a warning when equipping an oxygen pack that will be inefficient

---
+++ Source/HarmonyPatches/JobDriver_ForceTargetWear_TryMakePreToilReservations_Patch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(JobDriver_ForceTargetWear), nameof(JobDriver_ForceTargetWear.TryMakePreToilReservations))]
+public static class JobDriver_ForceTargetWear_TryMakePreToilReservations_Patch
+{
+    private static void Postfix(JobDriver_ForceTargetWear __instance, bool __result)
+    {
+        if (!__result)
+            return;
+
+        var apparel = __instance.Apparel;
+        var comp = apparel.GetComp<CompApparelOxygenProvider>();
+        if (comp == null || comp.Props.minResistanceToActivate <= 0)
+            return;
+
+        var target = __instance.TargetPawn;
+        var baseVacuumResistance = StatDefOf.VacuumResistance.Worker.GetValueUnfinalized(StatRequest.For(target), false);
+        if (baseVacuumResistance < comp.Props.minResistanceToActivate)
+            Messages.Message("VGE_EquippingOnOtherIneffectiveOxygenPack".Translate(__instance.pawn.Named("PAWN"), target.Named("TARGET"), apparel.Named("APPAREL")), new LookTargets(__instance.pawn, target, apparel), MessageTypeDefOf.CautionInput, false);
+    }
+}
---
+++ Source/HarmonyPatches/JobDriver_Wear_TryMakePreToilReservations_Patch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(JobDriver_Wear), nameof(JobDriver_Wear.TryMakePreToilReservations))]
+public static class JobDriver_Wear_TryMakePreToilReservations_Patch
+{
+    private static void Postfix(JobDriver_Wear __instance, bool __result)
+    {
+        if (!__result)
+            return;
+
+        var apparel = __instance.Apparel;
+        var comp = apparel.GetComp<CompApparelOxygenProvider>();
+        if (comp == null || comp.Props.minResistanceToActivate <= 0)
+            return;
+
+        var baseVacuumResistance = StatDefOf.VacuumResistance.Worker.GetValueUnfinalized(StatRequest.For(__instance.pawn), false);
+        if (baseVacuumResistance < comp.Props.minResistanceToActivate)
+            Messages.Message("VGE_EquippingIneffectiveOxygenPack".Translate(__instance.pawn.Named("PAWN"), apparel.Named("APPAREL")), new LookTargets(__instance.pawn, apparel), MessageTypeDefOf.CautionInput, false);
+    }
+}
