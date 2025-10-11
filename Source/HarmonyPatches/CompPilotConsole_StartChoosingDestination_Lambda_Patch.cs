b0332226dd7f5396f4316ca59691201bc1cf6321
SokyranTheDragon <exploder1531@gmail.com>
2025-10-11 18:18:49 +0200

    Fix flight cost mentioning chemfuel instead of astrofuel

---
+++ Source/HarmonyPatches/CompPilotConsole_StartChoosingDestination_Lambda_Patch.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch]
+public static class CompPilotConsole_StartChoosingDestination_Lambda_Patch
+{
+    private static bool Prepare(MethodBase method)
+    {
+        if (method != null)
+            return true;
+        if (TargetMethod() != null)
+            return true;
+
+        Log.Error("[VGE] Error replacing Chemfuel with Astrofuel text - could not find one of the lambdas to CompPilotConsole:StartChoosingDestination.");
+        return false;
+    }
+
+    private static MethodBase TargetMethod() => typeof(CompPilotConsole).FindIncludingInnerTypes<MethodBase>(t => t.FirstMethod(m => m.Name == "<StartChoosingDestination_NewTemp>b__2"));
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instr)
+    {
+        var targetField = typeof(ThingDefOf).DeclaredField(nameof(ThingDefOf.Chemfuel));
+        var replacementField = typeof(VGEDefOf).DeclaredField(nameof(VGEDefOf.VGE_Astrofuel));
+
+        var replacements = 0;
+
+        foreach (var ci in instr)
+        {
+            if (ci.LoadsField(targetField))
+            {
+                ci.operand = replacementField;
+                replacements++;
+            }
+
+            yield return ci;
+        }
+
+        const int expectedReplacements = 1;
+        if (replacements != expectedReplacements)
+            Log.Error($"[VGE] Patching CompPilotConsole:StartChoosingDestination failed - unexpected amount of patches. Expected patches: {expectedReplacements}, actual patch amount: {replacements}. Selecting gravship destination may incorrectly refer to astrofuel as chemfuel.");
+    }
+}
