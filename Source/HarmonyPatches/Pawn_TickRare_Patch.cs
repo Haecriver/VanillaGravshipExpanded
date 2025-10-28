cbac26b2612e2f0031bf1ef6e4f71b18544f1736
SokyranTheDragon <exploder1531@gmail.com>
2025-10-28 01:38:01 +0100

    Added constant oxygen drain for pawns

---
+++ Source/HarmonyPatches/Pawn_TickRare_Patch.cs
@@ -0,0 +1,29 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(Pawn), nameof(Pawn.TickRare))]
+public static class Pawn_TickRare_Patch
+{
+    private const float ChangePerInterval = (float)GenTicks.TickRareInterval / GenTicks.TicksPerRealSecond * 0.01f;
+
+    private static void Postfix(Pawn __instance)
+    {
+        var map = __instance.Map;
+        if (map is { Biome.inVacuum: true } && __instance.def.race.Humanlike && !__instance.RaceProps.IsMechanoid && (!__instance.IsMutant || __instance.mutant.Def.breathesAir))
+        {
+            var resistance = __instance.GetStatValue(StatDefOf.VacuumResistance, cacheStaleAfterTicks: 60);
+            if (resistance >= 1f)
+                return;
+
+            var room = __instance.GetRoom();
+            if (room is not { Vacuum: < 1f })
+                return;
+
+            var change = 100f / room.CellCount * (1f - resistance) * (ChangePerInterval * 0.2f);
+            room.Vacuum += change;
+        }
+    }
+}
