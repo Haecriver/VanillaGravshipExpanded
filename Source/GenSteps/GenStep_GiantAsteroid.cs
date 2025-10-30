21a8f6f407bda02de64d32091e88afd4b33db3e3
juanosarg <31105173+juanosarg@users.noreply.github.com>
2025-10-30 13:52:44 +0100

    Assorted fixes

--- Source/GenSteps/GenStep_GiantAsteroid.cs
+++ Source/GenSteps/GenStep_GiantAsteroid.cs
@@ -13,5 +13,66 @@ namespace VanillaGravshipExpanded
     public class GenStep_GiantAsteroid : GenStep_Asteroid
     {
         public override float Radius => 0.35f;
+
+        public override void Generate(Map map, GenStepParams parms)
+        {
+           
+                GenerateAsteroidElevation(map, parms);
+                GenerateCaveElevation(map, parms);
+                SpawnGiantAsteroid(map);
+                SpawnOres(map, parms);
+                if (Rand.Chance(ruinsChance))
+                {
+                    GenerateRuins(map, parms);
+                }
+                if (Rand.Chance(archeanTreeChance))
+                {
+                    GenerateArcheanTree(map, parms);
+                }
+                map.OrbitalDebris = OrbitalDebrisDefOf.Asteroid;
+            
+        }
+
+        private static void SpawnGiantAsteroid(Map map)
+        {
+            using (map.pathing.DisableIncrementalScope())
+            {
+                foreach (IntVec3 allCell in map.BoundsRect(5).Cells)
+                {
+                    float num = MapGenerator.Elevation[allCell];
+                    float num2 = MapGenerator.Caves[allCell];
+                    if (num > 0.5f)
+                    {
+                        map.terrainGrid.SetTerrain(allCell, ThingDefOf.Vacstone.building.naturalTerrain);
+                    }
+                    if (num > 0.7f && num2 == 0f)
+                    {
+                        GenSpawn.Spawn(ThingDefOf.Vacstone, allCell, map);
+                    }
+                    if (num > 0.7f)
+                    {
+                        map.roofGrid.SetRoof(allCell, RoofDefOf.RoofRockThin);
+                    }
+                }
+                HashSet<IntVec3> mainIsland = new HashSet<IntVec3>();
+                map.floodFiller.FloodFill(map.Center, (IntVec3 x) => x.GetTerrain(map) != TerrainDefOf.Space, delegate (IntVec3 x)
+                {
+                    mainIsland.Add(x);
+                });
+                foreach (IntVec3 allCell2 in map.AllCells)
+                {
+                    if (mainIsland.Contains(allCell2))
+                    {
+                        continue;
+                    }
+                    map.terrainGrid.SetTerrain(allCell2, TerrainDefOf.Space);
+                    map.roofGrid.SetRoof(allCell2, null);
+                    foreach (Thing item in allCell2.GetThingList(map).ToList())
+                    {
+                        item.Destroy();
+                    }
+                }
+            }
+        }
     }
 }
