cabef8e50ac223cd60f7a3bae1a8e2826c6453c3
Taranchuk <nfhfyxer@gmail.com>
2025-10-30 18:06:09 +0200

    scen part fix

--- Source/ScenParts/ScenPart_SpawnMechDestroyers.cs
+++ Source/ScenParts/ScenPart_SpawnMechDestroyers.cs
@@ -8,89 +8,90 @@ namespace VanillaGravshipExpanded
     [HotSwappable]
     public class ScenPart_SpawnMechDestroyers : ScenPart
     {
-        public override void PostMapGenerate(Map map)
+        public override void GenerateIntoMap(Map map)
         {
-            base.PostMapGenerate(map);
-            if (map.IsStartingMap is false) return;
-            List<KCSG.StructureLayoutDef> destroyerLayouts = new List<KCSG.StructureLayoutDef>
+            if (Find.GameInitData != null)
             {
-                VGEDefOf.VGE_MechOrbitalDestroyer_Alpha,
-                VGEDefOf.VGE_MechOrbitalDestroyer_Beta,
-                VGEDefOf.VGE_MechOrbitalDestroyer_Gamma
-            };
-
-            IntVec3 playerStartSpot = MapGenerator.PlayerStartSpot;
-            int mapSizeX = map.Size.x;
-            int mapSizeZ = map.Size.z;
-            List<CellRect> placedRects = new List<CellRect>();
-
-            for (int i = 0; i < 4; i++)
-            {
-                KCSG.StructureLayoutDef layout = destroyerLayouts.RandomElement();
-                int layoutWidth = layout.Sizes.x;
-                int layoutHeight = layout.Sizes.z;
-
-                IntVec3 spawnSpot = IntVec3.Invalid;
-                int attempts = 0;
-                while (!spawnSpot.IsValid && attempts < 200)
+                List<KCSG.StructureLayoutDef> destroyerLayouts = new List<KCSG.StructureLayoutDef>
                 {
-                    int x = Rand.Range(20, mapSizeX - layoutWidth - 20);
-                    int z = Rand.Range(20, mapSizeZ - layoutHeight - 20);
-                    CellRect rect = new CellRect(x, z, layoutWidth, layoutHeight);
-                    if (rect.Contains(playerStartSpot) || rect.Overlaps(CellRect.CenteredOn(playerStartSpot, 50)))
-                    {
-                        attempts++;
-                        continue;
-                    }
-                    bool overlapsWithExisting = false;
-                    foreach (CellRect placedRect in placedRects)
-                    {
-                        CellRect expandedRect = new CellRect(placedRect.minX - 5, placedRect.minZ - 5, placedRect.Width + 10, placedRect.Height + 10);
-                        if (rect.Overlaps(expandedRect))
-                        {
-                            overlapsWithExisting = true;
-                            break;
-                        }
-                    }
+                    VGEDefOf.VGE_MechOrbitalDestroyer_Alpha,
+                    VGEDefOf.VGE_MechOrbitalDestroyer_Beta,
+                    VGEDefOf.VGE_MechOrbitalDestroyer_Gamma
+                };
 
-                    if (overlapsWithExisting)
-                    {
-                        attempts++;
-                        continue;
-                    }
+                IntVec3 playerStartSpot = MapGenerator.PlayerStartSpot;
+                int mapSizeX = map.Size.x;
+                int mapSizeZ = map.Size.z;
+                List<CellRect> placedRects = new List<CellRect>();
 
-                    bool canSpawn = true;
-                    foreach (IntVec3 cell in rect)
-                    {
-                        if (!cell.InBounds(map) || cell.Fogged(map) || cell.Roofed(map))
-                        {
-                            canSpawn = false;
-                            break;
-                        }
-                    }
-                    if (canSpawn)
-                    {
-                        spawnSpot = rect.Min;
-                        placedRects.Add(rect);
-                    }
-                    attempts++;
-                }
-
-                if (spawnSpot.IsValid)
+                for (int i = 0; i < 4; i++)
                 {
-                    CellRect spawnRect = new CellRect(spawnSpot.x, spawnSpot.z, layoutWidth, layoutHeight);
-                    LayoutUtils.CleanRect(layout, map, spawnRect, true);
-                    layout.Generate(spawnRect, map, Faction.OfMechanoids);
-                    foreach (IntVec3 cell in spawnRect)
+                    KCSG.StructureLayoutDef layout = destroyerLayouts.RandomElement();
+                    int layoutWidth = layout.Sizes.x;
+                    int layoutHeight = layout.Sizes.z;
+
+                    IntVec3 spawnSpot = IntVec3.Invalid;
+                    int attempts = 0;
+                    while (!spawnSpot.IsValid && attempts < 200)
                     {
-                        foreach (Thing thing in map.thingGrid.ThingsListAt(cell))
+                        int x = Rand.Range(20, mapSizeX - layoutWidth - 20);
+                        int z = Rand.Range(20, mapSizeZ - layoutHeight - 20);
+                        CellRect rect = new CellRect(x, z, layoutWidth, layoutHeight);
+                        if (rect.Contains(playerStartSpot) || rect.Overlaps(CellRect.CenteredOn(playerStartSpot, 50)))
                         {
-                            if (thing is Building_TurretGun turret && turret.Faction == Faction.OfMechanoids)
+                            attempts++;
+                            continue;
+                        }
+                        bool overlapsWithExisting = false;
+                        foreach (CellRect placedRect in placedRects)
+                        {
+                            CellRect expandedRect = new CellRect(placedRect.minX - 5, placedRect.minZ - 5, placedRect.Width + 10, placedRect.Height + 10);
+                            if (rect.Overlaps(expandedRect))
                             {
-                                var dormantComp = turret.GetComp<CompCanBeDormant>();
-                                if (dormantComp != null)
+                                overlapsWithExisting = true;
+                                break;
+                            }
+                        }
+
+                        if (overlapsWithExisting)
+                        {
+                            attempts++;
+                            continue;
+                        }
+
+                        bool canSpawn = true;
+                        foreach (IntVec3 cell in rect)
+                        {
+                            if (!cell.InBounds(map) || cell.Fogged(map) || cell.Roofed(map))
+                            {
+                                canSpawn = false;
+                                break;
+                            }
+                        }
+                        if (canSpawn)
+                        {
+                            spawnSpot = rect.Min;
+                            placedRects.Add(rect);
+                        }
+                        attempts++;
+                    }
+
+                    if (spawnSpot.IsValid)
+                    {
+                        CellRect spawnRect = new CellRect(spawnSpot.x, spawnSpot.z, layoutWidth, layoutHeight);
+                        LayoutUtils.CleanRect(layout, map, spawnRect, true);
+                        layout.Generate(spawnRect, map, Faction.OfMechanoids);
+                        foreach (IntVec3 cell in spawnRect)
+                        {
+                            foreach (Thing thing in map.thingGrid.ThingsListAt(cell))
+                            {
+                                if (thing is Building_TurretGun turret && turret.Faction == Faction.OfMechanoids)
                                 {
-                                    dormantComp.wakeUpOnTick = Find.TickManager.TicksGame + 30000;
+                                    var dormantComp = turret.GetComp<CompCanBeDormant>();
+                                    if (dormantComp != null)
+                                    {
+                                        dormantComp.wakeUpOnTick = Find.TickManager.TicksGame + 30000;
+                                    }
                                 }
                             }
                         }
