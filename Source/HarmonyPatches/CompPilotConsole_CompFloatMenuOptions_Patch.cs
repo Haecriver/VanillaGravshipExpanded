ec904ae81307513bd55a81639b5b5228baa8f12a
SokyranTheDragon <exploder1531@gmail.com>
2025-11-12 17:39:06 +0100

    Use the correct ship name when right-clicking consoles

---
+++ Source/HarmonyPatches/CompPilotConsole_CompFloatMenuOptions_Patch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(CompPilotConsole), nameof(CompPilotConsole.CompFloatMenuOptions))]
+public static class CompPilotConsole_CompFloatMenuOptions_Patch
+{
+    private static IEnumerable<FloatMenuOption> Postfix(IEnumerable<FloatMenuOption> options, CompPilotConsole __instance)
+    {
+        var pilotGravshipText = ((string)"PilotGravship".Translate()).TrimEnd();
+    
+        foreach (var option in options)
+        {
+            if (option.Label == pilotGravshipText)
+            {
+                if (__instance.parent.def == VGEDefOf.VGE_PilotCockpit)
+                    option.Label = "VGE_PilotGravjumper".Translate();
+                else if (__instance.parent.def == VGEDefOf.VGE_PilotBridge)
+                    option.Label = "VGE_PilotGravhulk".Translate();
+            }
+    
+            yield return option;
+        }
+    }
+}
