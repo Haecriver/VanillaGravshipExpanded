c3394a217e36ae3cc04aec6b72c795342a2fdcb0
SokyranTheDragon <exploder1531@gmail.com>
2025-11-30 17:16:44 +0100

    Extra safety for Vehicle Framework patch

--- Source/HarmonyPatches/ModCompatability/VehicleFramework_GenGridVehicles_ImpassableForVehicles_Patch.cs
+++ Source/HarmonyPatches/ModCompatability/VehicleFramework_GenGridVehicles_ImpassableForVehicles_Patch.cs
@@ -8,8 +8,19 @@ namespace VanillaGravshipExpanded;
 [HarmonyPatch]
 public static class VehicleFramework_GenGridVehicles_ImpassableForVehicles_Patch
 {
-    private static bool Prepare() => ModLister.AnyModActiveNoSuffix(["SmashPhil.VehicleFramework"]);
-    
+    private static bool Prepare(MethodBase method)
+    {
+        if (method != null)
+            return true;
+        if (!ModLister.AnyModActiveNoSuffix(["SmashPhil.VehicleFramework"]))
+            return false;
+        if (TargetMethod() != null)
+            return true;
+
+        Log.Error($"[VGE] Failed initializing Vehicle Framework compatibility. Could not find target method: \"Vehicles.GenGridVehicles:ImpassableForVehicles\"");
+        return false;
+    }
+
     public static MethodBase TargetMethod()
     {
         return AccessTools.Method("Vehicles.GenGridVehicles:ImpassableForVehicles");
