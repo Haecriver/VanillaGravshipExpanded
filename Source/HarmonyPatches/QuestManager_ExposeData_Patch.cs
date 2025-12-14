084d10beaeaf0387f4c6cba03ce2b8812fcd70a2
SokyranTheDragon <exploder1531@gmail.com>
2025-12-14 16:11:59 +0100

    Added neverending Gravship quest
    - The Gravship quest won't end after finishing mechhive
    - On load, if the Gravship quest was marked as finished, marked it as not finished

---
+++ Source/HarmonyPatches/QuestManager_ExposeData_Patch.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(QuestManager), nameof(QuestManager.ExposeData))]
+public static class QuestManager_ExposeData_Patch
+{
+    private static void Prefix(QuestManager __instance)
+    {
+        // During PostLoadInit, quests get sorted into either active or historical quests.
+        // Set the quest as not ended (and restore end outcome to default value) so it gets re-added as an active one.
+        if (Scribe.mode == LoadSaveMode.PostLoadInit)
+        {
+            Quest firstInactive = null;
+
+            foreach (var quest in __instance.QuestsListForReading)
+            {
+                if (quest != null && quest.root == QuestScriptDefOf.GravEngine)
+                {
+                    // We have an active quest, return
+                    if (!quest.ended)
+                        return;
+                    firstInactive ??= quest;
+                }
+            }
+
+            if (firstInactive != null)
+            {
+                firstInactive.ended = false;
+                firstInactive.endOutcome = default;
+            }
+        }
+    }
+}
---
+++ Source/HarmonyPatches/QuestNode_Root_Gravcore_Mechhive_RunInt_Patch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using RimWorld;
+using RimWorld.QuestGen;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(QuestNode_Root_Gravcore_Mechhive), nameof(QuestNode_Root_Gravcore_Mechhive.RunInt))]
+public static class QuestNode_Root_Gravcore_Mechhive_RunInt_Patch
+{
+    private static void Postfix()
+    {
+        var quest = QuestGen.quest;
+        if (quest == null)
+        {
+            Log.Error("[VGE] QuestGen.quest is null after QuestNode_Root_Gravcore_Mechhive:RunInt method.");
+            return;
+        }
+
+        // Reverse for loop, since we're removing elements from the list (in RemovePart method).
+        // We could directly remove from the list (and set the part's parent to null),
+        // but for safety call RemoveParent in case some mods patch it to do something with the removed part.
+        for (var i = quest.PartsListForReading.Count - 1; i >= 0; i--)
+        {
+            var part = quest.PartsListForReading[i];
+            if (part is QuestPart_QuestEndParent)
+                quest.RemovePart(part);
+        }
+    }
+}
