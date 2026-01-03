12d953cf7ac0fa36aeec1282b8e80cc30ab50b8b
Taranchuk <nfhfyxer@gmail.com>
2026-01-03 09:01:15 +0200

    bunk bed, big top turret lightning overlay, heatsink power transmit fixes

--- Source/HarmonyPatches/TurretTop_DrawTurret_Patch.cs
+++ Source/HarmonyPatches/TurretTop_DrawTurret_Patch.cs
@@ -1,4 +1,5 @@
 using HarmonyLib;
+using LudeonTK;
 using RimWorld;
 using UnityEngine;
 using Verse;
@@ -9,15 +10,35 @@ namespace VanillaGravshipExpanded
     [HarmonyPatch(typeof(TurretTop), "DrawTurret")]
     public static class TurretTop_DrawTurret_Patch
     {
-        public static void Prefix(TurretTop __instance, ref Vector3 drawLoc)
+        public static bool Prefix(TurretTop __instance, Vector3 drawLoc, Vector3 recoilDrawOffset, float recoilAngleOffset)
         {
             if (__instance.parentTurret is Building_GravshipTurret || __instance.parentTurret.def == VGEDefOf.VGE_PointDefenseTurret)
             {
+                Building_Turret parentTurret = __instance.parentTurret;
+                Vector3 v = new Vector3(parentTurret.def.building.turretTopOffset.x, 0f, parentTurret.def.building.turretTopOffset.y).RotatedBy(__instance.CurRotation);
+                Vector3 pos = drawLoc + Altitudes.AltIncVect + v;
                 if (WorldComponent_GravshipController.CutsceneInProgress is false)
                 {
-                    drawLoc.y += 6.19f;
+                    pos.y += 6.19f;
+                }
+                Map map = __instance.parentTurret.Map;
+                if (map != null)
+                {
+                    float turretTopDrawSize = parentTurret.def.building.turretTopDrawSize;
+                    v = v.RotatedBy(recoilAngleOffset);
+                    v += recoilDrawOffset;
+                    float num = parentTurret.CurrentEffectiveVerb?.AimAngleOverride ?? __instance.CurRotation;
+                    Quaternion q = ((float)TurretTop.ArtworkRotation + num).ToQuat();
+                    Matrix4x4 matrix = Matrix4x4.TRS(pos, q, new Vector3(turretTopDrawSize, 1f, turretTopDrawSize));
+                    MaterialPropertyBlock block = new MaterialPropertyBlock();
+                    Color skyColor = map.skyManager.CurSky.colors.sky;
+                    skyColor.a = 1f;
+                    block.SetColor("_Color", skyColor);
+                    Graphics.DrawMesh(MeshPool.plane10, matrix, parentTurret.TurretTopMaterial, 0, null, 0, block);
+                    return false;
                 }
             }
+            return true;
         }
     }
 }
