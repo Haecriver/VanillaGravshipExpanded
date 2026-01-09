7a0eab59a8ed11c9afab74c3723619d2e576aeab
Taranchuk <nfhfyxer@gmail.com>
2026-01-09 18:01:20 +0200

    Building_AnticraftCaster aim cone fix

---
+++ Source/HarmonyPatches/GenDraw_DrawAimPie_Patch.cs
@@ -0,0 +1,18 @@
+using HarmonyLib;
+using Verse;
+
+namespace VanillaGravshipExpanded
+{
+    [HotSwappable]
+    [HarmonyPatch(typeof(GenDraw), nameof(GenDraw.DrawAimPie))]
+    public static class GenDraw_DrawAimPie_Patch
+    {
+        public static void Prefix(Thing shooter, ref float offsetDist)
+        {
+            if (shooter is Building_AnticraftCaster)
+            {
+                offsetDist += 5f;
+            }
+        }
+    }
+}
