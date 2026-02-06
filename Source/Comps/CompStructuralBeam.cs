019910d43188f7491c6d9dd2259eef14bebec66f
Taranchuk <nfhfyxer@gmail.com>
2026-02-06 13:02:52 +0200

    beam destroy code tweaks

--- Source/Comps/CompStructuralBeam.cs
+++ Source/Comps/CompStructuralBeam.cs
@@ -14,20 +14,18 @@ namespace VanillaGravshipExpanded
     public class CompStructuralBeam : ThingComp
     {
         private static HashSet<Thing> collapsingBeams = new HashSet<Thing>();
-
         public override void PostDestroy(DestroyMode mode, Map map)
         {
             base.PostDestroy(mode, map);
-
-            if (mode == DestroyMode.KillFinalize && !collapsingBeams.Contains(parent))
+            if (!collapsingBeams.Contains(parent))
             {
                 collapsingBeams.Clear();
                 collapsingBeams.Add(parent);
-                TriggerAdjacentCollapse(map);
+                TriggerAdjacentCollapse(map, mode);
             }
         }
 
-        private void TriggerAdjacentCollapse(Map map)
+        private void TriggerAdjacentCollapse(Map map, DestroyMode mode)
         {
             if (map == null) return;
 
@@ -43,7 +41,7 @@ namespace VanillaGravshipExpanded
                     if (things[j] is Building building && building.def == parent.def && !building.Destroyed && !collapsingBeams.Contains(building))
                     {
                         collapsingBeams.Add(building);
-                        building.Destroy(DestroyMode.KillFinalize);
+                        building.Destroy(mode);
                     }
                 }
             }
