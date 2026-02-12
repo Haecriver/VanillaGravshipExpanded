e440fd3deb8f95eedc78905402d9813cfabf179a
SokyranTheDragon <exploder1531@gmail.com>
2026-02-12 16:18:40 +0100

    Compact biosculpter net

---
+++ Source/HarmonyPatches/CompBiosculpterPod_PowerOn_Patch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(CompBiosculpterPod), nameof(CompBiosculpterPod.PowerOn), MethodType.Getter)]
+public static class CompBiosculpterPod_PowerOn_Patch
+{
+    private static bool Prefix(CompBiosculpterPod __instance, ref bool __result)
+    {
+        // If power comp is present, let it run
+        if (__instance.parent.GetComp<CompPowerTrader>() != null)
+            return true;
+
+        // No power comp, return early and set result to true
+        __result = true;
+        return false;
+    }
+}
---
+++ Source/HarmonyPatches/CompBiosculpterPod_SetPower_Patch.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+using RimWorld;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(CompBiosculpterPod), nameof(CompBiosculpterPod.SetPower))]
+public static class CompBiosculpterPod_SetPower_Patch
+{
+    private static bool Prefix(CompBiosculpterPod __instance)
+    {
+        // Check if power comp is present, prevent the method from running if it's missing.
+        return (__instance.powerComp ??= __instance.parent.GetComp<CompPower>()) != null;
+    }
+}
