87f075b654c6cbb0dfbeb111b594e02918b7e760
SokyranTheDragon <exploder1531@gmail.com>
2026-03-26 17:29:16 +0100

    Reset vac barrier roof color when it's replaced/removed

---
+++ Source/HarmonyPatches/RoofGrid_SetRoof.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using VEF.Maps;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(RoofGrid), nameof(RoofGrid.SetRoof))]
+public static class RoofGrid_SetRoof_Patch
+{
+    private static void Postfix(IntVec3 c, RoofDef def, Map ___map)
+    {
+        if (def == null || def.GetModExtension<RoofExtension>()?.customRoofGraphic is ColorableVacBarrierRoofGraphic)
+            ___map.SetVacBarrierRoofColorAt(c, VacBarrierRoofUtility.BaseColor);
+    }
+}
