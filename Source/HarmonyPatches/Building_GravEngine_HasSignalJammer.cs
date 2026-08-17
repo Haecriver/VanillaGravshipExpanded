e35fbe33a1bc0f4564f47016469d1ddfef1e9b40
SokyranTheDragon <exploder1531@gmail.com>
2026-08-17 15:27:16 +0200

    Add a DefModExtension to give gravship components additional types (rather than just 1)

---
+++ Source/HarmonyPatches/Building_GravEngine_HasSignalJammer.cs
@@ -0,0 +1,31 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(Building_GravEngine), nameof(Building_GravEngine.HasSignalJammer), MethodType.Getter)]
+[HarmonyPatchCategory(GravshipsMod.HarmonyLatePatchCategory)]
+public static class Building_GravEngine_HasSignalJammer_Patch
+{
+    public static bool isPatchActive = false;
+
+    private static bool Prepare() => isPatchActive;
+
+    private static void Postfix(Building_GravEngine __instane, ref bool __result)
+    {
+        // Don't do anything if an actual signal jammer is present
+        if (__result)
+            return;
+
+        for (var i = 0; i < __instane.GravshipComponents.Count; i++)
+        {
+            var extension = __instane.GravshipComponents[i].parent.def.GetModExtension<AdditionalGravshipComponentTypesExtension>();
+            if (extension != null && extension.additionalComponentTypeDefs.NotNullAndContains(GravshipComponentTypeDefOf.SignalJammer))
+            {
+                __result = true;
+                return;
+            }
+        }
+    }
+}
---
+++ Source/HarmonyPatches/Building_GravEngine_MissingComponents_Patch.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace VanillaGravshipExpanded;
+
+[HarmonyPatch(typeof(Building_GravEngine), nameof(Building_GravEngine.MissingComponents), MethodType.Getter)]
+[HarmonyPatchCategory(GravshipsMod.HarmonyLatePatchCategory)]
+public static class Building_GravEngine_MissingComponents_Patch
+{
+    public static bool isPatchActive = false;
+
+    private static bool Prepare() => isPatchActive;
+
+    private static void Prefix(Building_GravEngine __instance, ref bool __state) => __state = __instance.missingComponentsDirty;
+
+    private static void Postfix(Building_GravEngine __instance, List<GravshipComponentTypeDef> __result, bool __state)
+    {
+        // Only re-check if component list was dirty before the main method was called
+        if (!__state)
+            return;
+
+        // Remove all components that vanilla code added, but we had a multipurpose component present for it
+        for (var i = __result.Count - 1; i >= 0; i--)
+        {
+            for (var j = 0; j < __instance.GravshipComponents.Count; j++)
+            {
+                var extension = __instance.GravshipComponents[j].parent.def.GetModExtension<AdditionalGravshipComponentTypesExtension>();
+                if (extension != null && extension.additionalComponentTypeDefs.NotNullAndContains(__result[i]))
+                {
+                    __result.RemoveAt(i);
+                    break;
+                }
+            }
+        }
+    }
+}
